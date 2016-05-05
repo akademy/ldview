9eaab2c33c3d4f2e26bf404978c29b2595ff46bb
Matthew Wilcoxson <bitbucket@akademy.co.uk>
2016-05-05 21:52:02 +0100

    Handle "funny" variable names and entity label

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,29 @@ router.get('/', function(req, res, next) {
 			if (err) {
 				throw err;
 			}
-			res.render('data/list', { entities: result });
+			res.render('data/list', {
+				entities: result,
+				hasValue: function(chunk, context, bodies, params) {
+					var entity = context.current();
+					return params.key in entity;
+				},
+				value: function(chunk, context, bodies, params) {
+					var entity = context.current();
+					return chunk.write( entity[params.key] );
+				},
+				entityName: function(chunk, context, bodies, params) {
+					var entity = context.current(),
+						name = "No label";
+
+					if( "rdfs:label" in entity ) {
+						name = entity["rdfs:label"];
+					}
+					else if( "foaf:name"  in entity ) {
+						name = entity["foaf:name"];
+					}
+					return chunk.write( name );
+				}
+			});
 		});
 	});
 });
