b85bd2269618914a7e7b041e3c549b029ce55c0e
Matthew Wilcoxson <matthew.wilcoxson@oerc.ox.ac.uk>
2016-05-11 19:16:14 +0100

    Move functions to external file.

--- routes/entities.js
+++ routes/entities.js
@@ -5,9 +5,10 @@ var mongodb = require('mongodb');
 var MongoClient = mongodb.MongoClient;
 
 var config = require('../config/config');
+var helpersDust = require('../lib/helpersDust.js');
 
 /* list all the entities we have */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res /*, next */) {
 	MongoClient.connect(config.local.databaseUrl, function(err, db) {
 		if (err) {
 			throw err;
@@ -18,26 +19,9 @@ router.get('/', function(req, res, next) {
 			}
 			res.render('entities/list', {
 				entities: result,
-				hasValue: function(chunk, context, bodies, params) {
-					var entity = context.current();
-					return params.key in entity;
-				},
-				value: function(chunk, context, bodies, params) {
-					var entity = context.current();
-					return chunk.write( entity[params.key] );
-				},
-				entityName: function(chunk, context, bodies, params) {
-					var entity = context.current(),
-						name = "No label";
-
-					if( "rdfs:label" in entity ) {
-						name = entity["rdfs:label"];
-					}
-					else if( "foaf:name"  in entity ) {
-						name = entity["foaf:name"];
-					}
-					return chunk.write( name );
-				}
+				//hasValue: helpersDust.hasValue,
+				value: helpersDust.value,
+				entityName: helpersDust.entityName
 			});
 		});
 	});
@@ -46,7 +30,7 @@ router.get('/', function(req, res, next) {
 /**
  * A *smart* view of an entity... hopefully...
  */
-router.get('/:id', function(req, res, next) {
+router.get('/:id', function(req, res /*, next*/) {
 	MongoClient.connect(config.local.databaseUrl, function(err, db) {
 		if (err) {
 			throw err;
@@ -57,18 +41,22 @@ router.get('/:id', function(req, res, next) {
 			}
 
 			res.render('entities/entity', {
-				result: (results.length > 0) ? results[0] : null,
-				keyValues : function(chunk, context, bodies, params) {
+				entity: (results.length > 0) ? results[0] : null,
+				keyValues : function(chunk, context /*, bodies, params*/ ) {
 					var entity = context.current();
 					var data = [];
 					for( var key in entity ) {
-						data.push( {
-							key : key,
-							value : entity[key]
-						} );
+						if( entity.hasOwnProperty(key) ) {
+							data.push( {
+								key : key,
+								value : entity[key]
+							} );
+						}
 					}
 					return data;
-				}
+				},
+				value: function(a,b,c,d) { console.log(a,b,c,d); return helpersDust.value(a,b,c,d); },
+				entityName: helpersDust.entityName
 			} );
 		});
 	});
@@ -78,7 +66,7 @@ router.get('/:id', function(req, res, next) {
 /**
  * A raw - simple - view of this entity
  */
-router.get('/raw/:id', function(req, res, next) {
+router.get('/raw/:id', function(req, res/*, next*/) {
 	MongoClient.connect(config.local.databaseUrl, function(err, db) {
 		if (err) {
 			throw err;
@@ -90,14 +78,16 @@ router.get('/raw/:id', function(req, res, next) {
 
 			res.render('entities/raw', {
 				result: (results.length > 0) ? results[0] : null,
-				keyValues : function(chunk, context, bodies, params) {
+				keyValues : function(chunk, context /*, bodies, params*/) {
 					var entity = context.current();
 					var data = [];
 					for( var key in entity ) {
-						data.push( {
-							key : key,
-							value : entity[key]
-						} );
+						if( entity.hasOwnProperty(key) ) {
+							data.push({
+								key: key,
+								value: entity[key]
+							});
+						}
 					}
 					return data;
 				}
