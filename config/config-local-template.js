2c20b55ad6bb087980f16ecae69fb777abe5873b
Matthew Wilcoxson <matthew.wilcoxson@oerc.ox.ac.uk>
2016-08-18 16:38:15 +0100

    Add template for config file

--- config/config-local-template.js
+++ config/config-local-template.js
@@ -1,5 +1,11 @@
 /* Make a copy of this and rename to "config-local.js", fill in the blanks. */
 module.exports = {
 	databaseUrl: /\?/,        // e.g. "mongodb://localhost:port/db"
-	port: /\?/               // e.g. 3001
+	port: /\?/,               // e.g. 3001
+	
+	fuseki : {
+		host : /\?/,          // "localhost",
+		port : /\?/,          //"3030",
+		dataset : /\?/        // "test1"
+	}
 };
