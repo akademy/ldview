01f4d57e7c61a5a8a55fdff5e1b32a5aa96be6fe
Matthew Wilcoxson <matthew.wilcoxson@oerc.ox.ac.uk>
2016-11-24 18:41:38 +0000

    Updated entities views.

--- routes/entities.js
+++ routes/entities.js
@@ -6,6 +6,7 @@ var MongoClient = mongodb.MongoClient;
 
 var config = require('../config/config');
 var helpersDust = require('../lib/helpersDust.js');
+var helpersEntity = require('../lib/helpersEntity.js');
 
 /* list all the entities we have */
 router.get('/', function(req, res /*, next */) {
@@ -40,24 +41,58 @@ router.get('/:id', function(req, res /*, next*/) {
 				throw err;
 			}
 
-			res.render('entities/entity', {
-				entity: (results.length > 0) ? results[0] : null,
-				keyValues : function(chunk, context /*, bodies, params*/ ) {
-					var entity = context.current();
-					var data = [];
-					for( var key in entity ) {
-						if( entity.hasOwnProperty(key) ) {
-							data.push( {
-								key : key,
-								value : entity[key]
-							} );
-						}
-					}
-					return data;
-				},
-				value: function(a,b,c,d) { console.log(a,b,c,d); return helpersDust.value(a,b,c,d); },
-				entityName: helpersDust.entityName
-			} );
+			var result = (results.length > 0) ? results[0] : null;
+			
+			var links = helpersEntity.entityLinks( result );
+			var fields = Object.keys(links);
+			var query = [];
+
+			for( var l=0; l<fields.length; l++) {
+				// That is looping through
+				var field = fields[l];
+				var values = result[field];
+				if( typeof values !== "array" ) {
+					values = [values];
+				}
+				
+				for(var v=0; v<values.length; v++) {
+					var props = values[v]["@id"].split("/");
+
+					query.push({
+						"annal:type_id": props[0],
+						"annal:id": props[1]
+					})
+				}
+			}
+
+			db.collection(config.collection)
+				.find({
+					$or: query
+				})
+				.toArray(function ( err, associated ) {
+
+					res.render('entities/entity', {
+						entity: result,
+						keyValues: function (chunk, context /*, bodies, params*/) {
+							var entity = context.current();
+							var data = [];
+							for (var key in entity) {
+								if (entity.hasOwnProperty(key)) {
+									data.push({
+										key: key,
+										value: entity[key]
+									});
+								}
+							}
+							return data;
+						},
+						value: function (a, b, c, d) {
+							console.log(a, b, c, d);
+							return helpersDust.value(a, b, c, d);
+						},
+						entityName: helpersDust.entityName
+					});
+				});
 		});
 	});
 });
