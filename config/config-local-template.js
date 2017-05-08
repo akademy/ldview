2f1e7cc06bae5d8644d2baf43b559953ad8599a1
Matthew Wilcoxson <git@akademy.co.uk>
2017-05-08 12:22:29 +0100

    Tweak

--- config/config-local-template.js
+++ config/config-local-template.js
@@ -1,6 +1,6 @@
 /* Make a copy of this and rename to "config-local.js", fill in the blanks. */
 module.exports = {
-	databaseUrl: /\?/,        // e.g. "mongodb://localhost:port/db"
+	databaseUrl: /\?/,        // e.g. "mongodb://localhost:port/db",
 	port: /\?/,               // e.g. 3001
 	
 	fuseki : {
